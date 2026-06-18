1d31121d6a2e82d109583b97f9c8d6ee8d35f47a
Louie Diamos <louiemonitoring@gmail.com>
2026-06-18 15:40:02 +0800

    fix: refractor Edit functionality and fix session state issues, and minor typos

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -20,8 +20,6 @@ if 'portfolio' not in st.session_state:
             columns=['Ticker', 'Shares', 'Avg_Buy_Price', 'Date_Added']
       )
 
-portfolio = st.session_state.portfolio
-
 # Sidebar
 st.sidebar.header('Add New Stock')
 
@@ -40,8 +38,8 @@ with st.sidebar.form('add_stock_form'):
         if not ticker:
             st.error('Please enter a Ticker Symbol')
             # Check for duplicate        
-        elif not portfolio.empty and ticker in portfolio['Ticker'].values:
-            st.warning(f'{ticker} already exixts in your portfolio. Please use ✏️ Edit button.')
+        elif not st.session_state.portfolio.empty and ticker in st.session_state.portfolio['Ticker'].values:
+            st.warning(f'{ticker} already exists in your portfolio. Please use the ✏️ Edit button.')
         else:
             # Create a new row and add it
             new_row = pd.DataFrame({
@@ -53,30 +51,30 @@ with st.sidebar.form('add_stock_form'):
             st.session_state.portfolio = pd.concat([st.session_state.portfolio,
                                                     new_row], ignore_index=True)
             st.success(f'Success! ✅ {ticker} has been added.')
-            time.sleep(2)
+            time.sleep(1)
             st.rerun()
         
 # ====== Main Content =======
 
-if not portfolio.empty:
+if not st.session_state.portfolio.empty:
     # Calculate all metrics
-    portfolio = calculate_portfolio_metrics(portfolio)
+    st.session_state.portfolio = calculate_portfolio_metrics(st.session_state.portfolio)
 
     # Summary Metrics
-    total_value = portfolio['Current_Value'].sum()
-    total_cost = portfolio['Cost_Basis'].sum()
+    total_value = st.session_state.portfolio['Current_Value'].sum()
+    total_cost = st.session_state.portfolio['Cost_Basis'].sum()
     total_gain = total_value - total_cost
-    total_gain_pct = (total_value-total_cost/ total_cost* 100) if total_cost> 0 else 0
+    total_gain_pct = (total_gain / total_cost * 100) if total_cost > 0 else 0
 
     col1, col2, col3, col4 = st.columns(4)
     col1.metric('Total Value', f'${total_value:.2f}')
-    col2.metric('Total Gain/Loss', f'${total_gain:.2f}')
-    col3.metric('Holdings', len(portfolio))
+    col2.metric('Total Gain/Loss', f'${total_gain:.2f}', f'{total_gain_pct:.2f}%')
+    col3.metric('Holdings', len(st.session_state.portfolio))
     col4.metric('Cash', '$0.00')
 
     # Portfolio Table
     st.subheader('Portfolio Holdings')
-    display_df = portfolio[['Ticker', 'Shares', 'Avg_Buy_Price', 'Current_Price',
+    display_df = st.session_state.portfolio[['Ticker', 'Shares', 'Avg_Buy_Price', 'Current_Price',
                             'Current_Value', 'Gain_Loss', 'Gain_Loss_%']].round(2)
 
     st.dataframe(
@@ -101,13 +99,13 @@ if not portfolio.empty:
     tab1, tab2 = st.tabs(['Allocation', 'Performance'])
 
     with tab1:
-        fig_pie = px.pie(portfolio, values='Current_Value', names='Ticker',
+        fig_pie = px.pie(st.session_state.portfolio, values='Current_Value', names='Ticker',
                          title='Portfolio Allocation')
         st.plotly_chart(fig_pie, width="stretch")
     
     with tab2:
         st.subheader('30-Day Portfolio Performance')
-        tickers = portfolio['Ticker'].tolist()
+        tickers = st.session_state.portfolio['Ticker'].tolist()
         hist_data = get_historical_data(tickers, period ='1mo')
         if not hist_data.empty:
             fig_hist = px.line(hist_data, title='Historical Price Movement (last 30 Days)')
@@ -116,7 +114,7 @@ else:
     st.info('👋 Your Portfolio is empty. Add some holdings using the sidebar!')
 
 # ====== Manage Holdings =======
-if not portfolio.empty:
+if not st.session_state.portfolio.empty:
     st.subheader('Manage Holdings')
     ticker_list = st.session_state.portfolio['Ticker'].unique()
     selected_ticker = st.selectbox('Select Ticker', ticker_list)
@@ -124,28 +122,31 @@ if not portfolio.empty:
     col1, col2 = st.columns(2)
     with col1:
         if st.button('🗑️ Delete', type='primary'):
-            st.session_state.portfolio = portfolio[portfolio['Ticker']!=selected_ticker].reset_index(drop=True)
+            st.session_state.portfolio = st.session_state.portfolio[
+                st.session_state.portfolio['Ticker']!=selected_ticker].reset_index(drop=True)
             st.success(f'{selected_ticker} has been deleted.')
-            time.sleep(2)
+            time.sleep(1)
             st.rerun()
 
     with col2:
         if st.button('✏️ Edit'):
-            row = portfolio[portfolio['Ticker'] == selected_ticker].iloc[0]
-            new_shares = st.number_input('Shares', value=float(row['Shares']))
-            new_price = st.number_input('Avg Buy Price', value=float(row['Avg_Buy_Price']))
+            st.session_state['editing'] = selected_ticker
 
+    if st.session_state.get('editing') == selected_ticker:
+            row = st.session_state.portfolio[st.session_state.portfolio['Ticker'] == selected_ticker].iloc[0]
+            new_shares = st.number_input('Shares', value=float(row['Shares']), key='edit_shares')
+            new_price = st.number_input('Avg_Buy_Price', value=float(row['Avg_Buy_Price']),key='edit_price')
+            
             if st.button('Save Changes'):
-                portfolio.loc[portfolio['Ticker'] == selected_ticker, 'Shares'] = new_shares
-                portfolio.loc[portfolio['Ticker'] == selected_ticker, 'Avg_Buy_Price'] = new_price
-                portfolio.to_csv(DATA_FILE, index=False)
-                time.sleep(2)
-                st.success('Updated successfull!')
-                time.sleep(2)
+                st.session_state.portfolio.loc[st.session_state.portfolio['Ticker'] == selected_ticker, 'Shares'] = new_shares
+                st.session_state.portfolio.loc[st.session_state.portfolio['Ticker'] == selected_ticker, 'Avg_Buy_Price'] = new_price
+                del st.session_state['editing']
+                st.success('Updated successfully!')
+                time.sleep(1)
                 st.rerun()
 
 # Export
-if not portfolio.empty: 
+if not st.session_state.portfolio.empty: 
     csv_data = st.session_state.portfolio.to_csv(index=False).encode('utf-8')
     st.download_button(
         label = '🗂️ Export file(.csv)',
